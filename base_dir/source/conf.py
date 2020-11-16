ecacabac05a989cce910dbd26dba7fc4402e92e8
juan <juanzox@gmail.com>
2020-11-16 20:35:39 -0300

    xw

--- base_dir/source/conf.py
+++ base_dir/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'jz-web'
+project = 'Juan Zamora O.'
 copyright = '2020, j.z.o.'
 author = 'j.z.o.'
 html_title = "Juan Zamora O."
@@ -50,12 +50,38 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'nature'
+#html_theme = 'nature'
 
-html_sidebars = {'**':['localtoc.html','searchbox.html']}
+#html_sidebars = {'**':['localtoc.html','searchbox.html']}
+#html_sidebars = {}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "logo01.jpg"
-html_style = "mystyle.css"
+#html_style = "mystyle.css"
+pygments_style = 'sphinx'
+#html_theme = 'sphinxdoc'
+#html_theme = 'insegel' # muy bueno
+#html_theme = 'renku' # igual buioen
+
+html_theme = 'sunpy'
+import sunpy_sphinx_theme
+html_theme_path = sunpy_sphinx_theme.get_html_theme_path()
+
+#import sphinx_redactor_theme
+#html_theme = 'sphinx_redactor_theme'
+#html_theme_path = [sphinx_redactor_theme.get_html_theme_path()]
+
+html_theme_options = {
+    'description': 'ABCDE.',
+    'github_repo': 'jfzo',
+    'nosidebar': False,
+    'header_searchbox': False,
+    'rightsidebar': False,
+}
+todo_include_todos = False
+html_use_index = False
+html_sidebars = {}
+html_show_sourcelink = False
--- base_dir/source/index.rst
+++ base_dir/source/index.rst
@@ -3,9 +3,10 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome
+WhoAmI
 *******
-Welcome to my site!
+I'm currently working as an associate professor at the Institute of statistics
+of Pontificia Universidad Católica de Valparaíso.
 
 About me
 ========
